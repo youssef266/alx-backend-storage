5906b25614a05fb4b8131934fd88ea7e85f3a2f9
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-12 18:55:05 +0300

    alx

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+--0x00-MySQL_Advanced/2-country_users.sql
+-- SQL script that creates a table users following these
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);

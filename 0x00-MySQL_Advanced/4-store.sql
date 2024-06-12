ccc376f9bfe446018ab3dd6d42dc3d0d12dfd759
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-13 01:03:29 +0300

    alx

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,13 +1,4 @@
--- Source:
--- Title: Store
-DELIMITER //
-
-CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE id = NEW.item_id;
-END //
-
-DELIMITER ;
+--  creates a trigger that decreases the quantity of an item after adding
+--  Quantity in the table items can be negative.
+CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;

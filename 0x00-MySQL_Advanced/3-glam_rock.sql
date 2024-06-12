5d994f2e85806ff5b9ed1bfd1a048fa34384cfcb
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-13 01:00:32 +0300

    alx

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,3 @@
--- Create a temporary table to store the computed lifespan
-CREATE TEMPORARY TABLE temp_lifespan AS
-SELECT band_name, 
-    YEAR(SUBSTRING_INDEX(lifespan, '-', 1)) AS formed,
-    CASE WHEN lifespan LIKE '%present%' THEN 2022 - YEAR(SUBSTRING_INDEX(lifespan, '-', 1))
-         ELSE YEAR(SUBSTRING_INDEX(lifespan, '-', -1)) - YEAR(SUBSTRING_INDEX(lifespan, '-', 1))
-    END AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%';
-
--- List bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, lifespan
-FROM temp_lifespan
-ORDER BY lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
